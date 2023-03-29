682269eb90d6cb42fd150f79813383b057ce6cc2
Raul Castro <raul.de.almeida.neto@accenture.com>
2023-03-28 22:05:33 -0300

    adicionando inputstream

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,14 +1,22 @@
 import javax.imageio.ImageIO;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
 
 public class GeradoraDeFigurinhas {
     
     public void cria() throws Exception{
         // leitura da imagem
-        BufferedImage imagemOriginal = ImageIO.read(new File("assets/filme.jpg"));
+        //InputStream inputStream = new FileInputStream(new File("assets/filme.jpg"));
+        InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_7.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
         
         // cria nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
@@ -20,7 +28,13 @@ public class GeradoraDeFigurinhas {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
         
-        // escrever uma frase na nova imagem 
+        // configurar a fonte
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(font);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 0, novaAltura - 100);
 
         // escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File("assets/figurinha.png"));

7a299201df56ddd255790bb6726ba1d633565f30
Raul Castro <raul.de.almeida.neto@accenture.com>
2023-03-28 22:20:50 -0300

    refatorando e chamando geradoradefigurinhas em App

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -25,9 +27,17 @@ public class App {
         
         // exibir e manipular os dados
         for (Map<String,String> filme: listaDeFilmes){
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            String nomeArquivo = titulo + ".png";
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+            
             System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
             System.out.println();
         }
     }
--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,13 +1,11 @@
-import javax.imageio.ImageIO;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 
 public class GeradoraDeFigurinhas {
